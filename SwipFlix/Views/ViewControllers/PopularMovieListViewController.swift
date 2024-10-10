f0b3742658967a88477cf4496e2ee942636815bd
Nouraiz <taimournouraizz@gmail.com>
2024-10-10 20:06:17 +0500

    Initial commit: Set up project structure
    - Added project files and directories

--- SwipFlix/Views/ViewControllers/PopularMovieListViewController.swift
+++ SwipFlix/Views/ViewControllers/PopularMovieListViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class PopularMovieListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
