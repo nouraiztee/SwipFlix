fc9dba49e4d9e1f8a5741940cb8d3bb52758c9c8
Nouraiz <taimournouraizz@gmail.com>
2024-10-11 18:30:43 +0500

    Add SwiftFlix networkservice implementation

--- SwipFlix/Network/SwipFlixNetworkService.swift
+++ SwipFlix/Network/SwipFlixNetworkService.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+enum NetworkError: Error {
+    case invalidResponse
+    case decodingError
+}
+
+class SwipFlixNetworkService: NetworkService {
+    private let urlSession: URLSession
+    private let requestBuilder: RequestBuilder
+    
+    init(urlSession: URLSession, requestBuilder: RequestBuilder) {
+        self.urlSession = urlSession
+        self.requestBuilder = requestBuilder
+    }
+    
+    func request<T>(_ endpoint: Endpoint) async throws -> T where T : Decodable {
+        let urlRequest = try requestBuilder.buildRequest(for: endpoint)
+        
+        let (data, response) = try await urlSession.data(for: urlRequest)
+        
+        guard let httpResonse = response as? HTTPURLResponse, httpResonse.statusCode == 200 else {
+            throw NetworkError.invalidResponse
+        }
+        
+        let decodedResponse = try JSONDecoder().decode(T.self, from: data)
+        
+        return decodedResponse
+        
+    }
+}
